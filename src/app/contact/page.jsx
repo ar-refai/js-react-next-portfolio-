349e5f9d34c359466bc1be8b36f8af36fc31c602
Abdelrahman Elrefai <team.man00@gmail.com>
2024-03-11 17:01:01 +0200

    Start Contact Desigb

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,12 @@
 'use client'
-import React from 'react';
-import {motion} from 'framer-motion'; 
+import {motion, spring} from 'framer-motion'; 
 import ParticlesComponent from '@/components/Particles';
+
+
+import { useState } from "react";
+
 const ContactPage = () => {
+  const TEXT  = `CONTACT ME NOW!`
   return (
     <motion.div
       className="h-full"
@@ -10,7 +14,28 @@ const ContactPage = () => {
       animate={{y:'0%'}}
       transition = {{duration:1}}
     >
-      Contact Page
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <motion.div>
+
+          </motion.div>
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 text-7xl flex items-center">
+          {TEXT.split('').map((letter,i)=> (
+            <motion.span 
+            initial ={{opacity:[0.4,1]}} 
+            animate={{opacity:0.2}}
+            transition={{duration:1 ,repeat:Infinity , type: 'spring', stiffness: 100, damping: 11 ,delay:i*0.5}}
+            key={i}>
+              {letter}
+            </motion.span>
+          ))}
+        </div>
+      </div>
+      
+
       <div className='relative z-0'>
       <ParticlesComponent />
       </div>
