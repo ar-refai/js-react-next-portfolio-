6eb2d82c13fb60e37660de5cd614dcef78090211
Abdelrahman Elrefai <team.man00@gmail.com>
2024-03-16 20:07:36 +0200

    finall

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -18,8 +18,7 @@ const HeroText = ({ children }) => {
                         'scale3d(1.25,.85,1)',
                         'scale3d(.9,1.05,1)',
                         'scale3d(1,1,1)',
-                    ],
-                    border:'1px solid white'
+                    ]
                 })}}
         >
             {children}
